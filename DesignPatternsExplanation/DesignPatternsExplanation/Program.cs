70eb62763cfefaff415becfa812930a2c06648e1
Omar <fidelomar@outlok.com>
2022-07-19 01:05:37 -0500

    Singleton

---
+++ DesignPatternsExplanation/DesignPatternsExplanation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DesignPatternsExplanation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = Singleton.Log.Instance;
+            log.Save("og");
+            log.Save("cg");
+            log.Save("as");
+            log.Save("dg");          
+        }
+    }
+}
